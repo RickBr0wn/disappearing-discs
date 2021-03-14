49133402cb7c50c06906672772ec50ced62f7eda
Rick Brown <ricky.brown.00@gmail.com>
2021-03-14 15:52:18 +0000

    adds Scenes (Game, Main Menu, GameOver)

---
+++ disappearing-discs/Scenes/GameOverScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.swift
+//  disappearing-discs
+//
+//  Created by Rick Brown on 14/03/2021.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+  
+}
---
+++ disappearing-discs/Scenes/MainMenuScene.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuScene.swift
+//  disappearing-discs
+//
+//  Created by Rick Brown on 14/03/2021.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenuScene: SKScene {
+  
+}
